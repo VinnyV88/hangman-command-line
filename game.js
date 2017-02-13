f4439502b1f76aec17eb0236a9eb1387d4d21e5d
vincent_visconti <vinnyv88@gmail.com>
2017-02-12 22:45:46 -0500

    added some game logic and style.

--- game.js
+++ game.js
@@ -1,14 +1,16 @@
 var inquirer = require('inquirer');
+var chalk = require('chalk');
+var clear = require('clear');
+var figlet = require('figlet');
 var Word = require('./Word');
 
-var newWord;
+var gameWord;
 
 var hangman = {
 	wins: 0,
 	losses: 0,
 	guesses: 8,
 	userGuess: " ",
-	hits: 0,
 	hit: false,
 	miss: false,
 	repeat: false,
@@ -31,51 +33,111 @@ var hangman = {
 
   },
 
+	startGame: function(){
+
+		this.initial();
+		gameWord.displayWord();
+		this.guessPrompt();
+
+	},
+
+	displayHeader: function() {
+		//clear the terminal
+		process.stdout.write('\x1Bc');
+
+		figlet('BREAKING BAD :  HANGMAN', function(err, data) {
+				if (err) {
+						console.log('Something went wrong...');
+						console.dir(err);
+						return;
+				}
+				console.log(chalk.bold.green(data));
+		});
+	},
+
 	initial: function() {
-		var randWord = this.chooseRandomWord()
-		newWord = new Word(randWord);
-		newWord.pushLetters();
-		this.hit = false
-		this.miss = false
-	}
+		this.hit = false;
+		this.miss = false;
+		this.repeat = false;
+		var randWord = this.chooseRandomWord();
+		gameWord = new Word(randWord);
+		gameWord.pushLetters();
+	},
 
-} // close hangman object
-
-inquirer.prompt([
-  {type: "confirm",
-    name: "play",
-		default: true,
-    message: "Would you like to play a game of hangman?"}
-  ]).then(function(data){
-	  if (data.play) startGame();
-});
-
-function startGame(){
-
-	hangman.initial();
-
-	newWord.displayWord();
-	guessPrompt();
-
-};
-
-function guessPrompt() {
+	guessPrompt: function() {
 	
-	inquirer.prompt([
-	  {type: "input",
-	    name: "letter",
-	    message: "What letter would you like to try?"}
+		inquirer.prompt([
+			{type: "input",
+				name: "letter",
+				message: "What letter would you like to try?"}
 
-	]).then(function(data){
-		//check if letter was guessed previously, if not:
-		newWord.guessedLetters.push(data.letter);
-		newWord.displayWord();
-		if (newWord.checkWin()) {
-			console.log("winner!!");
-			return;
-		} else {
-			guessPrompt();
+		]).then(function(data){
+			hangman.userGuess = data.letter.toLowerCase();
+			if (hangman.validKey() && hangman.newLetter()) {
+				gameWord.guessedLetters.push(data.letter);
+				if (gameWord.checkWin()) {
+					hangman.wins++;
+					console.log("winner!!");
+					return;
+				} else {
+					gameWord.displayWord();
+					hangman.guessPrompt();
+				}
+
+			} else {
+				console.log(chalk.bold.red(hangman.message));
+				hangman.guessPrompt();
+			}
+			//check if letter is a hit or miss
+			//if miss subtract 1 from hangman.guesses
+			//if hangman.guess === 0 then loss++ and end game
+
+		});
+	},
+
+		validKey: function() {
+		if ((this.userGuess >= "a") && (this.userGuess <= "z")) {
+			this.message = " ";
+			this.invKey = false;
+			return true;
 		}
+		else {
+			this.message = "Invalid key pressed, try again!";
+			this.invKey = true;
+			return false;
+		}
+	},
 
-	});
-}
+	newLetter: function() {
+		if (gameWord.guessedLetters.indexOf(this.userGuess) >= 0) {
+			this.message = "You tried that letter already!";
+			this.repeat = true;
+			return false;
+		}
+		else {
+			this.repeat = false;
+			return true;
+		}
+	}
+	
+}; // close hangman object
+
+
+//----------------program starts executing here--------------------
+
+hangman.displayHeader();
+
+setTimeout(function() {
+inquirer.prompt([
+	{type: "confirm",
+		name: "play",
+		default: true,
+		message: "Would you like to play a game of hangman?"}
+	]).then(function(data){
+		if (data.play) hangman.startGame();
+});
+}, 1 * 1000);
+
+
+
+
